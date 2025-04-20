cf6fcdbc0bfe480711e88fdd8ff3542855625734
Evgeniya Pavlova <evgeniya.pavlova@quadcode.com>
2025-04-20 11:46:04 +0300

    first commit

---
+++ src/components/Tg.svelte
@@ -0,0 +1,8 @@
+<script>
+	import DiscordLogo from './DiscordLogo.svelte';
+</script>
+
+<a class="tg-block" id="tg-block" href="https://discord.gg/XwSpBrm7">
+	<DiscordLogo /> Any other questions?
+</a>
+
